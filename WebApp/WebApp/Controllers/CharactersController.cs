a48a31408abc163749845c134d4d2af943015e4d
jwiatr01 <j.wiatrak@hypermedia.pl>
2016-06-17 14:50:00 +0200

    Add Character and Player crud

---
+++ WebApp/WebApp/Controllers/CharactersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Core.Data;
+
+namespace WebApp.Controllers
+{
+    public class CharactersController : Controller
+    {
+        private MainDB db = new MainDB();
+
+        // GET: Characters
+        public ActionResult Index()
+        {
+            var characters = db.Characters.Include(c => c.Players);
+            return View(characters.ToList());
+        }
+
+        // GET: Characters/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Characters characters = db.Characters.Find(id);
+            if (characters == null)
+            {
+                return HttpNotFound();
+            }
+            return View(characters);
+        }
+
+        // GET: Characters/Create
+        public ActionResult Create()
+        {
+            ViewBag.PlayerId = new SelectList(db.Players, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: Characters/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,PlayerId,HealthPoint,StaminaPoint,Attack,Deffense")] Characters characters)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Characters.Add(characters);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PlayerId = new SelectList(db.Players, "Id", "FirstName", characters.PlayerId);
+            return View(characters);
+        }
+
+        // GET: Characters/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Characters characters = db.Characters.Find(id);
+            if (characters == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PlayerId = new SelectList(db.Players, "Id", "FirstName", characters.PlayerId);
+            return View(characters);
+        }
+
+        // POST: Characters/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,PlayerId,HealthPoint,StaminaPoint,Attack,Deffense")] Characters characters)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(characters).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PlayerId = new SelectList(db.Players, "Id", "FirstName", characters.PlayerId);
+            return View(characters);
+        }
+
+        // GET: Characters/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Characters characters = db.Characters.Find(id);
+            if (characters == null)
+            {
+                return HttpNotFound();
+            }
+            return View(characters);
+        }
+
+        // POST: Characters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Characters characters = db.Characters.Find(id);
+            db.Characters.Remove(characters);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
